ab072fc8a758bda596343e889d20a09dd1495d8b
Keri Schreckengost <keris@live.com>
2017-07-30 16:24:13 -0600

    Basic Card: prompt working, adding to txt file working Review Cards: working - pulling data from txt file

--- play.js
+++ play.js
@@ -7,14 +7,19 @@ var inquirer = require("inquirer");
 //require fs npm to read and write questions from a file questions.txt
 var fs = require("fs");
 
+
+
 //prompt to start - create basic, create cloze, or play
-inquirer.prompt([
+newGame();
+function newGame() {
+	inquirer.prompt([
 	{
 		type: "list",
 		name: "start",
 		message: "What would you like to do?",
-		choices: ["New Basic Card", "New Cloze Card", "Play!"]
-	}])
+		choices: ["New Basic Card", "New Cloze Card", "Review Cards", "Play!"]
+	}
+	])
 	.then(function(answers){
 		//response user picks
 		var response = answers.start;
@@ -27,8 +32,56 @@ inquirer.prompt([
 			case "New Cloze Card":
 				newCloze();
 			break;
+			case "Review Cards":
+				review();
 			case "Play!":
 				play()
 			break;
 		}
+	})
+};
+//new Basic Cards
+function newBasic(){
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "questionBasic",
+			message: "What should the 'front' of the card ask?"
+		},
+		{
+			type: "input",
+			name: "answerBasic",
+			message: "What should the 'back' of the card say?"
+		}
+	])
+	.then(function (basic){
+		console.log("Question: " + basic.questionBasic);
+		console.log("Answer: " + basic.answerBasic);
+		console.log("-------------------");
+		addCards("Front: " + basic.questionBasic + "\r\n\r\n");
+		addCards("Back: " + basic.answerBasic +"\r\n\r\n");
+		newGame();
+	})
+};
+
+//new Cloze Cards
+
+//append cards to cards.txt file
+function addCards(userEntry){
+	fs.appendFile("cards.txt", userEntry, function(err){
+		if (err){
+			return console.log(err);
+		}
 	});
+}
+//Review Cards
+function review(){
+	fs.readFile("cards.txt", "utf8", function(err, data){
+		if (err){
+			return console.log(err);
+		}
+		else{
+			console.log(data);
+		}
+})};
+//Play!
